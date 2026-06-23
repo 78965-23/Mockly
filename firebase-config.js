50389c276cb16733710075085116284f5a4bd847
78965-23 <mukulbegha@gmail.com>
2026-06-23 18:45:36 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // firebase-config.js
-// Initialize Firebase with your project credentials
+// Firebase configuration for Mockly (mockly2-fe6bc)
 const firebaseConfig = {
-    apiKey: "AIzaSyDRcgEc428zLbEDVP8LmN7DPubfwTFhN9U",
-    authDomain: "heartspace-app.firebaseapp.com",
-    projectId: "heartspace-app",
-    storageBucket: "heartspace-app.firebasestorage.app",
-    messagingSenderId: "706297788927",
-    appId: "1:706297788927:web:4c2805d8e434aaddd70fca",
-    measurementId: "G-95B3ZV9P5H"
+  apiKey: "AIzaSyC3RBe6AlCewKXkcVS4cDEXLDbClTvBgBY",
+  authDomain: "mockly2-fe6bc.firebaseapp.com",
+  projectId: "mockly2-fe6bc",
+  storageBucket: "mockly2-fe6bc.firebasestorage.app",
+  messagingSenderId: "535116640494",
+  appId: "1:535116640494:web:4a02c00886fff2572503ff",
+  measurementId: "G-VGL50BKX19"
 };
 
 // Initialize Firebase
